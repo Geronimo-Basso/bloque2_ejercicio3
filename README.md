ff31ae478d95e0742d104a405cdddc2780cdb365
Geronimo Basso <gerobasso01@gmail.com>
2023-01-02 11:36:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Escribe un programa en el que se manejen un total de 5 pilas. La entrada de datos será de pares de enteros (i, j), tal que 1<= abs(1) <= n. De tal forma que el criterio de selección de pila sea:
+• Si i es positivo, debe introducirse el elemento j en la Pila i.
+• Si i es negativo, debe eliminarse el elemento j en la Pila j.
+• Si i es 0, fin del proceso de entrada.
+Los datos de entrada se introducen por teclado. Cuando termine el proceso, el programa debe escribir el contenido de las pilas en pantalla.
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "PilaGen.h"
+using namespace std;
+
+int main() {
+    PilaGen<int> *pilaI = new PilaGen<int>;
+    PilaGen<int> *pilaJ = new PilaGen<int>;
+    bool condicion = true;
+    int opcionMenu , i , j;
+    cout << "Bienvenido a nuestro programa, dependiendo de un par de número enteros (i,j) ingregado podremos realizaer diferentes tareas" << endl;
+    while ( condicion ){
+        cout << "Opciones Menu: \n1. Añadir par de números  \n2. Mostar contenido de las pilas  \n3. Terminar programa" << endl;
+        cin >> opcionMenu;
+        switch ( opcionMenu ) {
+            case 1:
+            {
+                cout << "Introduzca i: " << endl;
+                cin >> i;
+                pilaI->insertar_cabeza( i );
+                cout << "Introduzca j: " << endl;
+                cin >> j;
+                pilaJ->insertar_cabeza( j );
+                if ( i > 0 ){ //i es positivo
+                    pilaI->insertar_cabeza( j );
+                }else if ( i == 0 ){ //i igual menos q  cero
+                    cout << "------------- PILA i -------------------" << endl;
+                    for(int k = 0 ; k < pilaI->size() ; k++ ){
+                        cout << pilaI->pop( k ) << endl;
+                    }
+                    cout << "------------- PILA j -------------------" << endl;
+                    for(int m = 0 ; m < pilaJ->size() ; m++ ){
+                        cout << pilaJ->pop( m ) << endl;
+                    }
+                    break;
+                }else if( i < 0 ){ //i es 0
+                    pilaJ->eliminar_cabeza();
+                }
+                break;
+            }
+            case 2:
+            {
+                break;
+            }
+            case 3:
+            {
+                condicion = false;
+                break;
+            }
+            default :
+            {
+                cout << "ERROR, introduzca una opcion valida porfavor" << endl;
+            }
+        }
+    }
+    return 0;
+}
